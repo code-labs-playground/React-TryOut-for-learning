e2f47ff9deb315d6de455d1909bfcd91c8445afa
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-25 11:17:55 +0530

    Creating To Do List app by vite and setup it

---
+++ todo-list-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoList from "./TodoList.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <TodoList completedTodos={[]} incompleteTodos={[]} />
+    </>
+  )
+}
+
+export default App
