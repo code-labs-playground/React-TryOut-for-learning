e4057f306f7128f5e5e73fb047d7d7fe0f6a3f6f
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-26 22:38:21 +0530

    Add Redux and doing the State Management through it

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -3,39 +3,13 @@ import './App.css'
 import TodoList from './TodoList'
 
 function App() {
-    const [completedTodos, setCompletedTodos] = useState([
-        { text: 'Take out the garbage', isCompleted: true },
-        { text: 'Make dinner', isCompleted: true }
-    ]);
-    const [incompleteTodos, setIncompleteTodos] = useState([
-        { text: 'Paint the house', isCompleted: false }
-    ]);
-
-    function markToDoAsComplete(text) {
-        setIncompleteTodos(incompleteTodos
-            .filter(t=> t.text !==text));
-        setCompletedTodos([...completedTodos, {...incompleteTodos
-            .find(t => t.text === text), isCompleted: true
-        }]);
-    }
-
-    function deleteTodo(text) {
-        setCompletedTodos(completedTodos
-            .filter(t=> t.text !==text));
-    }
-
     function createTodo(text) {
         setIncompleteTodos([...incompleteTodos, { text, isCompleted: false}])
     }
 
     return (
         <>
-            <TodoList
-                completedTodos={completedTodos}
-                incompleteTodos={incompleteTodos}
-                onCompletedClicked={markToDoAsComplete}
-                onDeleteClicked={deleteTodo}
-                onCreateClicked={createTodo}/>
+            <TodoList />
         </>
     );
 }
--- todo-list-app/src/main.jsx
+++ todo-list-app/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { configureStore} from "@reduxjs/toolkit";
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import {todosSlice} from "./todoSlice";
+
+const store = configureStore({
+    reducer: {
+        todos: todosSlice.reducer,
+    },
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
 )
